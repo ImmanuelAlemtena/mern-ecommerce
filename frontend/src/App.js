cdd7a3249ea8cf326f7b8e2ae10a4671725ec10a
Immanuel Alemtena <immanuyeshi122@gmail.com>
2022-07-09 11:57:28 +0000

    created-frontend-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ecommerce</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
